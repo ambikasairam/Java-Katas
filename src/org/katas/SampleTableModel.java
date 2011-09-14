3555627684e7f1df704cae935b406a807521e205
BJ Peter DeLaCruz <thetravelingcs@gmail.com>
2011-09-13 22:11:04 -1000

    Added a program to test a subclass of DefaultTableModel.

---
+++ src/org/katas/SampleTableModel.java
@@ -0,0 +1,87 @@
+package org.katas;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ * Used to test how to use a table model with a JTable.
+ * 
+ * @author BJ Peter DeLaCruz
+ */
+public class SampleTableModel extends DefaultTableModel {
+
+  /** Used for object serialization. */
+  private static final long serialVersionUID = 1L;
+  /** Maximum number of columns. */
+  private static final int NUM_COLUMNS = 2;
+  /** Holds the data stored in all of the columns. */
+  private final Object[][] DATA;
+
+  /**
+   * Creates a new SampleTableModel object with a table that will have a user-specified number of
+   * rows and columns.
+   * 
+   * @param row Number of rows.
+   * @param col Number of columns.
+   */
+  public SampleTableModel(int row, int col) {
+    this.DATA = new Object[row][col];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setValueAt(Object value, int row, int col) {
+    this.DATA[row][col] = value;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Object getValueAt(int row, int col) {
+    return this.DATA[row][col];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public String getColumnName(int columnIndex) {
+    switch (columnIndex) {
+    case 0:
+      return "";
+    case 1:
+      return "Class";
+    default:
+      throw new IllegalArgumentException("Invalid column index.");
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean isCellEditable(int row, int col) {
+    return col == 0;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Class<?> getColumnClass(int columnIndex) {
+    if (columnIndex >= NUM_COLUMNS) {
+      throw new IllegalArgumentException("Invalid column index.");
+    }
+    return (columnIndex == 0) ? Boolean.class : String.class;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public int getColumnCount() {
+    return NUM_COLUMNS;
+  }
+}
---
+++ src/org/katas/TestTableModel.java
@@ -0,0 +1,67 @@
+package org.katas;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+/**
+ * Creates a table and adds rows and content to it using the custom table model.
+ * 
+ * @author BJ Peter DeLaCruz
+ */
+public class TestTableModel {
+
+  /**
+   * The main program.
+   * 
+   * @param args None.
+   */
+  public static void main(String... args) {
+    try {
+      UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+    }
+    catch (ClassNotFoundException e1) {
+      e1.printStackTrace();
+      return;
+    }
+    catch (InstantiationException e1) {
+      e1.printStackTrace();
+      return;
+    }
+    catch (IllegalAccessException e1) {
+      e1.printStackTrace();
+      return;
+    }
+    catch (UnsupportedLookAndFeelException e1) {
+      e1.printStackTrace();
+      return;
+    }
+
+    JFrame frame = new JFrame();
+    SampleTableModel tableModel = new SampleTableModel(5, 5);
+    tableModel.addRow(new Object[2]);
+    tableModel.addRow(new Object[2]);
+    tableModel.addRow(new Object[2]);
+    tableModel.addRow(new Object[2]);
+    tableModel.addRow(new Object[2]);
+    tableModel.setValueAt(true, 0, 0);
+    tableModel.setValueAt(false, 1, 0);
+    tableModel.setValueAt(true, 2, 0);
+    tableModel.setValueAt(false, 3, 0);
+    tableModel.setValueAt(true, 4, 0);
+    tableModel.setValueAt("ICS 690", 0, 1);
+    tableModel.setValueAt("ICS 613", 1, 1);
+    tableModel.setValueAt("ICS 611", 2, 1);
+    tableModel.setValueAt("ICS 432", 3, 1);
+    tableModel.setValueAt("ICS 413", 4, 1);
+    JTable table = new JTable(tableModel);
+    JScrollPane scrollPane = new JScrollPane(table);
+    frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+    frame.setSize(300, 150);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+  }
+}
