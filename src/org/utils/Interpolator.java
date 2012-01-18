2d6537f6d14f04b21e241ac4c6cffe05a33636c5
BJ Peter DeLaCruz <thetravelingcs@gmail.com>
2012-01-17 20:15:27 -1000

    Refactored Point class.

--- src/org/utils/Interpolator.java
+++ src/org/utils/Interpolator.java
@@ -12,57 +12,6 @@ import java.util.List;
  */
 public final class Interpolator {
 
-  /**
-   * A data point for a JFreeChart.
-   * 
-   * @author BJ Peter DeLaCruz <bjpeter@hawaii.edu>
-   * 
-   * @param <X> A Number representing an X-axis value.
-   * @param <Y> A Number representing a Y-axis value.
-   */
-  public static class Point<X extends Number, Y extends Number> {
-    private X xValue;
-    private Y yValue;
-
-    /** Creates a Point object with no values. */
-    public Point() {
-      // Empty constructor.
-    }
-
-    /**
-     * Creates a Point object that can be used to represent a data point on a JFreeChart.
-     * 
-     * @param xValue The X-axis value.
-     * @param yValue The Y-axis value.
-     */
-    public Point(X xValue, Y yValue) {
-      setValue(xValue, yValue);
-    }
-
-    /** @return The X-axis value. */
-    public X getX() {
-      return this.xValue;
-    }
-
-    /** @return The Y-axis value. */
-    public Y getY() {
-      return this.yValue;
-    }
-
-    /**
-     * Sets the X-axis and Y-axis values in this object.
-     * 
-     * @param xValue The X-axis value.
-     * @param yValue The Y-axis value.
-     */
-    public final void setValue(X xValue, Y yValue) {
-      Validator.checkNull(xValue);
-      Validator.checkNull(yValue);
-      this.xValue = xValue;
-      this.yValue = yValue;
-    }
-  }
-
   /** Don't instantiate this class. */
   private Interpolator() {
     // Empty constructor.
