63efbe36aa7e8ee93279895062833ea30dcde717
BJ Peter DeLaCruz <thetravelingcs@gmail.com>
2012-01-24 19:26:03 -1000

    Added a demo program to show possible rendering issue in JFreeChart. See http://www.jfree.org/forum/viewtopic.php?f=3&t=109659.

--- src/org/utils/StackedAreaChartDemo.java
+++ src/org/utils/StackedAreaChartDemo.java
@@ -1,5 +1,6 @@
 package org.utils;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
@@ -8,17 +9,20 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StackedXYAreaRenderer2;
 import org.jfree.data.time.FixedMillisecond;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.TimeSeriesDataItem;
+import org.jfree.data.time.TimeTableXYDataset;
 
 /**
  * Test class.
  * 
  * @author BJ Peter DeLaCruz <bjpeter@hawaii.edu>
  */
-public class Chart {
+public class StackedAreaChartDemo {
 
   /**
    * Test program that will display a JFreeChart showing interpolated data points.
@@ -27,6 +31,7 @@ public class Chart {
    * display both series.
    */
   public static void main(String... args) {
+    // Check arguments.
     if (args.length != 1) {
       System.err.println("Usage: java Chart [0|1|2]\n\n  -- 0: Display Series 1.");
       System.err.println("  -- 1: Display Series 2.\n  -- 2: Display both series.");
@@ -39,12 +44,14 @@ public class Chart {
       return;
     }
 
+    // Create some sample data.
     List<Point<Number, Number>> list1 = new ArrayList<Point<Number, Number>>();
-    list1.add(new Point<Number, Number>(100, 100));
-    list1.add(new Point<Number, Number>(200, 200));
-    list1.add(new Point<Number, Number>(300, 400));
-    list1.add(new Point<Number, Number>(400, 200));
-    list1.add(new Point<Number, Number>(500, 100));
+    list1.add(new Point<Number, Number>(50, 100.0));
+    list1.add(new Point<Number, Number>(150, 100));
+    list1.add(new Point<Number, Number>(250, 200));
+    list1.add(new Point<Number, Number>(350, 400));
+    list1.add(new Point<Number, Number>(450, 200));
+    list1.add(new Point<Number, Number>(550, 100));
 
     List<Point<Number, Number>> list2 = new ArrayList<Point<Number, Number>>();
     list2.add(new Point<Number, Number>(50, 100.0));
@@ -54,61 +61,52 @@ public class Chart {
     list2.add(new Point<Number, Number>(450, 400.0));
     list2.add(new Point<Number, Number>(550, 200.0));
 
-    List<List<Point<Number, Number>>> lists = new ArrayList<List<Point<Number, Number>>>();
-    List<Point<Number, Number>> extrapolatedList1 = new ArrayList<Point<Number, Number>>();
-    List<Point<Number, Number>> extrapolatedList2 = new ArrayList<Point<Number, Number>>();
-    if ("1".equals(option)) {
-      lists.add(list1);
-      extrapolatedList1 = Interpolator.interpolate(list1, lists);
-    }
-    else if ("2".equals(option)) {
-      lists.add(list2);
-      extrapolatedList2 = Interpolator.interpolate(list2, lists);
-    }
-    else if ("0".equals(option)) {
-      lists.add(list1);
-      lists.add(list2);
-      extrapolatedList1 = Interpolator.interpolate(list1, lists);
-      extrapolatedList2 = Interpolator.interpolate(list2, lists);
-    }
-
+    // Add data to time series.
     TimeSeries series1 = new TimeSeries("Series 1", FixedMillisecond.class);
-    for (Point<Number, Number> dataPoint : extrapolatedList1) {
+    for (Point<Number, Number> dataPoint : list1) {
       if ("1".equals(option) || "0".equals(option)) {
         series1.add(new FixedMillisecond(dataPoint.getX().longValue()), dataPoint.getY());
       }
-      System.err.println(dataPoint.getX() + "\t" + dataPoint.getY());
+      series1.setDescription("Series 1");
     }
 
-    System.err.println();
-
     TimeSeries series2 = new TimeSeries("Series 2", FixedMillisecond.class);
-    for (Point<Number, Number> dataPoint : extrapolatedList2) {
+    for (Point<Number, Number> dataPoint : list2) {
       if ("2".equals(option) || "0".equals(option)) {
         series2.add(new FixedMillisecond(dataPoint.getX().longValue()), dataPoint.getY());
       }
-      System.err.println(dataPoint.getX() + "\t" + dataPoint.getY());
+      series2.setDescription("Series 2");
     }
 
-    TimeSeriesCollection dataset = new TimeSeriesCollection();
+    TimeSeriesCollection collection = new TimeSeriesCollection();
     if ("1".equals(option)) {
-      dataset.addSeries(series1);
+      collection.addSeries(series1);
     }
     else if ("2".equals(option)) {
-      dataset.addSeries(series2);
+      collection.addSeries(series2);
     }
     else if ("0".equals(option)) {
-      dataset.addSeries(series1);
-      dataset.addSeries(series2);
+      collection.addSeries(series1);
+      collection.addSeries(series2);
     }
 
+    TimeTableXYDataset dataset = new TimeTableXYDataset();
+    @SuppressWarnings("unchecked")
+    List<TimeSeries> timeSeriesList = collection.getSeries();
+    for (TimeSeries t : timeSeriesList) {
+      for (int index = 0; index < t.getItemCount(); index++) {
+        TimeSeriesDataItem dataItem = (TimeSeriesDataItem) t.getItems().get(index);
+        dataset.add(t.getTimePeriod(index), dataItem.getValue().doubleValue(),
+            t.getDescription());
+      }
+    }
+
+    // Create and display chart.
     JFreeChart chart =
-        ChartFactory.createXYLineChart("Test", null, null, dataset, PlotOrientation.VERTICAL,
+        ChartFactory.createStackedXYAreaChart(null, null, null, dataset, PlotOrientation.VERTICAL,
             false, true, false);
 
-    XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) chart.getXYPlot().getRenderer();
-    renderer.setSeriesShapesVisible(0, true);
-    renderer.setSeriesShapesVisible(1, true);
+    customizeChart(chart);
 
     ChartPanel chartPanel = new ChartPanel(chart);
     chartPanel.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
@@ -118,6 +116,24 @@ public class Chart {
     frame.pack();
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
+  }
+
+  /**
+   * Customizes the JFreeChart.
+   * 
+   * @param chart The chart to customize.
+   */
+  private static void customizeChart(JFreeChart chart) {
+    StackedXYAreaRenderer2 renderer = (StackedXYAreaRenderer2) chart.getXYPlot().getRenderer();
+    renderer.setSeriesPaint(0, Color.RED);
+    renderer.setSeriesPaint(1, Color.BLUE);
+
+    XYPlot plot = chart.getXYPlot();
+    plot.getDomainAxis().setLowerMargin(0.0);
+    plot.getDomainAxis().setUpperMargin(0.0);
+    plot.setBackgroundPaint(Color.WHITE);
+    plot.setDomainGridlinePaint(Color.WHITE);
+    plot.setRangeGridlinePaint(Color.BLACK);
   }
 
 }
