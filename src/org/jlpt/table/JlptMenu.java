786430173210f7c704252b394bc62079650987e1
BJ Peter DeLaCruz <thetravelingcs@gmail.com>
2012-07-13 21:48:52 -1000

    Added utility class.

---
+++ src/org/jlpt/table/JlptMenu.java
@@ -0,0 +1,79 @@
+package org.jlpt.table;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JTable;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import org.jlpt.model.JapaneseEntry;
+import org.jlpt.utils.FileUtils;
+import org.jlpt.utils.Validator;
+
+/**
+ * A popup menu that is displayed when the user right-clicks on an entry in the table.
+ * 
+ * @author BJ Peter DeLaCruz
+ */
+@SuppressWarnings("serial")
+public class JlptMenu extends JPopupMenu {
+
+  private final JTable table;
+
+  /**
+   * Creates a new JlptMenu instance.
+   * 
+   * @param table The table that contains the entries of Japanese words and English meanings.
+   */
+  public JlptMenu(JTable table) {
+    super();
+    Validator.checkNull(table);
+
+    this.table = table;
+
+    JMenuItem exportCsvMenuItem = new JMenuItem("Export to CSV");
+    exportCsvMenuItem.addActionListener(new ExportAction());
+    add(exportCsvMenuItem);
+
+    // TODO: Add a Copy Entry to Clipboard menu item later.
+
+  }
+
+  /**
+   * An action that exports the table model to a CSV file on the user's computer.
+   * 
+   * @author BJ Peter DeLaCruz
+   */
+  private class ExportAction implements ActionListener {
+
+    /** {@inheritDoc} */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+      List<JapaneseEntry> entries = ((JlptTableModel) table.getModel()).getEntries();
+      List<String> lines = new ArrayList<>();
+      for (JapaneseEntry entry : entries) {
+        lines.add(entry.getJword() + "," + entry.getRomaji() + "," + entry.getEnglishMeaning());
+      }
+      JFileChooser chooser = new JFileChooser();
+      FileNameExtensionFilter filter =
+          new FileNameExtensionFilter("Comma-separated file (*.csv)", "csv");
+      chooser.setFileFilter(filter);
+      int returnValue = chooser.showSaveDialog(JlptMenu.this);
+      if (returnValue == JFileChooser.APPROVE_OPTION) {
+        try {
+          FileUtils.writeToFile(lines, chooser.getSelectedFile());
+        }
+        catch (IOException e) {
+          // TODO: Replace with logger.
+          System.err.println("There was a problem writing to the file.");
+        }
+      }
+    }
+
+  }
+
+}
---
+++ src/org/jlpt/utils/FileUtils.java
@@ -0,0 +1,147 @@
+package org.jlpt.utils;
+
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.event.MouseInputAdapter;
+import org.jlpt.model.JapaneseEntry;
+import org.jlpt.table.JlptMenu;
+import org.jlpt.table.JlptTableModel;
+
+/**
+ * Utility class that contains methods for reading from and writing to a file. Contains a
+ * <code>main</code> method that tests the <code>readFile</code> methods and displays the results in
+ * a JFrame.
+ * 
+ * @author BJ Peter DeLaCruz
+ */
+public final class FileUtils {
+
+  /** Do not instantiate this class. */
+  private FileUtils() {
+    // Empty constructor.
+  }
+
+  /**
+   * Reads in the file at the given URL.
+   * 
+   * @param url The URL of the file.
+   * @throws IOException If there are problems reading in the file.
+   * @return The list of strings (lines) read in from the file.
+   */
+  public static List<String> readFile(URL url) throws IOException {
+    Validator.checkNull(url);
+    return readFile(url.getFile());
+  }
+
+  /**
+   * Reads in the file at the given URL.
+   * 
+   * @param filename The name of the file.
+   * @throws IOException If there are problems reading in the file.
+   * @return The list of strings (lines) read in from the file.
+   */
+  public static List<String> readFile(String filename) throws IOException {
+    Validator.checkNotEmptyString(filename);
+
+    FileInputStream fstream = new FileInputStream(filename);
+    InputStreamReader in = new InputStreamReader(fstream, "UTF8");
+    BufferedReader reader = new BufferedReader(in);
+
+    List<String> lines = new ArrayList<>();
+    String currentLine = "";
+    while ((currentLine = reader.readLine()) != null) {
+      lines.add(currentLine);
+    }
+
+    reader.close();
+
+    return lines;
+  }
+
+  /**
+   * Writes the given list of strings (lines) to the given file.
+   * @param lines The lines to write to the file.
+   * @param file The file to write to.
+   * @throws IOException If there are problems writing to the file.
+   */
+  public static void writeToFile(List<String> lines, File file) throws IOException {
+    Validator.checkNotEmpty(lines);
+
+    FileOutputStream fstream = new FileOutputStream(file);
+    OutputStreamWriter out = new OutputStreamWriter(fstream, "UTF8");
+    BufferedWriter writer = new BufferedWriter(out);
+
+    for (String line : lines) {
+      writer.write(line);
+      writer.newLine();
+    }
+
+    writer.close();
+  }
+
+  /**
+   * Tests the <code>readFile</code> methods.
+   * 
+   * @param args None.
+   */
+  public static void main(String... args) {
+    try {
+      String filename = "C:\\Users\\BJ Peter DeLaCruz\\workspace\\Katas\\";
+      List<String> lines = readFile(filename + "src\\org\\jlpt\\utils\\japanese_words.csv");
+      List<JapaneseEntry> entries = new ArrayList<>();
+      for (String line : lines) {
+        String[] words = line.split(";");
+        if (words.length != 3) {
+          // Ignore entries that do not have exactly three fields.
+          continue;
+        }
+        JapaneseEntry entry = new JapaneseEntry(words[0], words[1], words[2]);
+        entries.add(entry);
+      }
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      JFrame frame = new JFrame();
+      frame.setTitle("JLPT Study v0.1");
+      JlptTableModel model = new JlptTableModel(entries);
+      final JTable table = new JTable(model);
+      table.setShowVerticalLines(false);
+      table.addMouseListener(new MouseInputAdapter() {
+
+        @Override
+        public void mouseClicked(MouseEvent event) {
+          if (SwingUtilities.isRightMouseButton(event)) {
+            JlptMenu menu = new JlptMenu(table);
+            menu.show(table, event.getX(), event.getY());
+          }
+        }
+
+      });
+      table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+      JScrollPane pane =
+          new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+              JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.add(pane);
+      frame.pack();
+      frame.setVisible(true);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
