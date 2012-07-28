88241b4f73f99518f8583cacf9ba32d5d1319f6f
BJ Peter DeLaCruz <thetravelingcs@gmail.com>
2012-07-27 20:16:29 -1000

    Used nested try-with-resources statements.

--- src/org/jlpt/server/main/Task.java
+++ src/org/jlpt/server/main/Task.java
@@ -1,6 +1,5 @@
 package org.jlpt.server.main;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -10,6 +9,8 @@ import java.util.logging.Logger;
 import org.jlpt.common.db.AddRemoveRequest;
 import org.jlpt.common.db.Commands;
 import org.jlpt.common.db.DbManager;
+import org.jlpt.common.db.EntryAlreadyExistsException;
+import org.jlpt.common.db.EntryDoesNotExistException;
 import org.jlpt.common.utils.Validator;
 
 /**
@@ -41,61 +42,41 @@ public class Task implements Runnable {
   /** {@inheritDoc} */
   @Override
   public void run() {
-    ObjectOutputStream ostream = null;
-    ObjectInputStream istream = null;
-    try {
-      // I can't use the new try-with-resources statement because the output stream
-      // needs to be flushed before the input stream is created.
-      ostream = new ObjectOutputStream(this.socket.getOutputStream());
+    try (ObjectOutputStream ostream = new ObjectOutputStream(this.socket.getOutputStream())) {
       ostream.flush();
-      istream = new ObjectInputStream(this.socket.getInputStream());
-      while (true) {
-        Object request = istream.readObject();
-        if (request == Commands.QUIT) {
-          break;
-        }
-        if (request instanceof AddRemoveRequest) {
-          AddRemoveRequest addRemoveRequest = (AddRemoveRequest) request;
-          switch (addRemoveRequest.getCommand()) {
-          case ADD:
-            this.databaseManager.addEntry(addRemoveRequest.getEntry());
-            LOGGER.log(Level.INFO, "Successfully added entry to database.");
+      try (ObjectInputStream istream = new ObjectInputStream(this.socket.getInputStream())) {
+        while (true) {
+          Object request = istream.readObject();
+          if (request == Commands.QUIT) {
             break;
-          case REMOVE:
-            this.databaseManager.removeEntry(addRemoveRequest.getEntry());
-            LOGGER.log(Level.INFO, "Successfully removed entry from database.");
-            break;
-          default:
-            throw new IllegalArgumentException("Invalid command: " + addRemoveRequest.getCommand());
           }
-        }
-        else if (request == Commands.GET) {
-          ostream.writeObject(this.databaseManager.getEntries());
+          if (request instanceof AddRemoveRequest) {
+            AddRemoveRequest addRemoveRequest = (AddRemoveRequest) request;
+            switch (addRemoveRequest.getCommand()) {
+            case ADD:
+              this.databaseManager.addEntry(addRemoveRequest.getEntry());
+              LOGGER.log(Level.INFO, "Successfully added entry to database.");
+              break;
+            case REMOVE:
+              this.databaseManager.removeEntry(addRemoveRequest.getEntry());
+              LOGGER.log(Level.INFO, "Successfully removed entry from database.");
+              break;
+            default:
+              throw new IllegalArgumentException("Invalid command: "
+                  + addRemoveRequest.getCommand());
+            }
+          }
+          else if (request == Commands.GET) {
+            ostream.writeObject(this.databaseManager.getEntries());
+          }
         }
       }
     }
-    catch (Exception e) {
+    catch (IOException | EntryAlreadyExistsException | EntryDoesNotExistException e) {
       LOGGER.log(Level.SEVERE, e.getMessage());
     }
-    finally {
-      closeStream(istream);
-      closeStream(ostream);
-    }
-  }
-
-  /**
-   * Closes an input or output stream.
-   * @param stream The stream to close.
-   */
-  private void closeStream(Closeable stream) {
-    if (stream == null) {
-      return;
-    }
-    try {
-      stream.close();
-    }
-    catch (IOException e) {
-      LOGGER.log(Level.SEVERE, "Unable to close input stream: " + e);
+    catch (Exception e) {
+      LOGGER.log(Level.SEVERE, e.getMessage());
     }
   }
 
